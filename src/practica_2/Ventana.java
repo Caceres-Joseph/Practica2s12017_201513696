fda598d73feaafa8af88181750ffecb835cc337f
joseph <joseph@joseph-HP-14-Notebook-PC>
2017-03-03 02:14:46 -0600

    Después de la calificación

---
+++ src/practica_2/Ventana.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica_2;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTabPane;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebView;
+
+/**
+ * FXML Controller class
+ *
+ * @author joseph
+ */
+public class Ventana implements Initializable {
+
+    @FXML
+    private JFXTextField lblListaBuscar;
+
+    @FXML
+    private JFXTextField lblListaAgregar;
+
+    @FXML
+    private JFXTextField lblListaBorrar;
+    
+    @FXML
+    private void btnListaBuscar(javafx.event.ActionEvent event) {
+    }
+
+    @FXML
+    private void btnListaAgregar(javafx.event.ActionEvent event) {
+    }
+
+    @FXML
+    private void btnListaBorrar(javafx.event.ActionEvent event) {
+    }
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+    
+
+}
